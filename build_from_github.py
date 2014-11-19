c162d705d651ba77e32bb159ae7a33f6c4702f4e
Jeremy Keeshin <jkeesh@gmail.com>
2014-11-18 16:46:52 -0800

    update static

--- build_from_github.py
+++ build_from_github.py
@@ -7,6 +7,7 @@ import os
 def start():
     os.system("git pull")
     os.system("python build_html.py introcs/setup/config.yaml introcs/build/book.html")
+    print "Completed build from github."
 
 if __name__ == "__main__":
     start()
